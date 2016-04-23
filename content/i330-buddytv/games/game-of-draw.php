dd47ba5c96db8b91e9d8d2fc84f914c23c9773b8
Kodlee Yin <fru1tstand@users.noreply.github.com>
2016-04-22 18:40:18 -0700

    Changed game of draw from no plugin to in game photo

--- content/i330-buddytv/games/game-of-draw.php
+++ content/i330-buddytv/games/game-of-draw.php
@@ -4,7 +4,7 @@ require_once PHP_ROOT . '/fmmsc/Setup.php';
 $body = <<<HTML
 <div class="container">
 	<div class="game-flash">
-		<img src="http://i.imgur.com/JYMOZWd.gif" />
+		<img src="http://i.imgur.com/5aSqUb8.png" />
 	</div>
 </div>
 HTML;
